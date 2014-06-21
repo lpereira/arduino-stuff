f66bc2ac88f11b797164b0e12250d0e6639ef6d3
Leandro Pereira <leandro@tia.mat.br>
2014-06-21 18:29:29 -0300

    Update firmware to new version
    Lots of stuff happened, mostly formatting fucked up by Arduino editor. But most importantly, the fade in/out table has been fixed so it now works as it should.

--- ElWireDance/ElWireDance.ino
+++ ElWireDance/ElWireDance.ino
@@ -6,6 +6,8 @@
 // Should be a power of 2!
 #define N_TASKS 4
 
+static const int dancerIdEepromAddress = 511;
+
 static const int bluetoothRxPin = 3;
 static const int bluetoothTxPin = 4;
 static const int ledPin = 13;
@@ -15,7 +17,7 @@ static int elWire2Pin = 6;
 // HC-06 Bluetooth class
 // @lafp, 18-01-2014
 class Bluetooth {
- public:
+public:
   enum BaudRate {
     BR_1200 = 1,
     BR_2400,
@@ -27,23 +29,40 @@ class Bluetooth {
   };
 
   Bluetooth(const int rx, const int tx)
-    : conn(rx, tx) {}
+: 
+    conn(rx, tx) {
+    }
 
-  void begin() { conn.begin(9600); }
+  void begin() { 
+    conn.begin(9600); 
+  }
 
-  void set_name(const char* name) { send_command("NAME", name); }
-  void set_pin(const char* pin) { send_command("PIN", pin); }
+  void set_name(const char* name) { 
+    send_command("NAME", name); 
+  }
+  void set_pin(const char* pin) { 
+    send_command("PIN", pin); 
+  }
   void set_baud_rate(const BaudRate r) {
-    char cmd[] = {(char) (r + '0'), '\0'};
+    char cmd[] = {
+      (char) (r + '0'), '\0'    };
     send_command("BAUD", cmd);
   }
 
-  bool available() { return conn.available(); }
-  int read() { return conn.read(); }
-  void write(const char c) { conn.write(c); }
-  void write(const char* s) { conn.write(s); }
+  bool available() { 
+    return conn.available(); 
+  }
+  int read() { 
+    return conn.read(); 
+  }
+  void write(const char c) { 
+    conn.write(c); 
+  }
+  void write(const char* s) { 
+    conn.write(s); 
+  }
 
- private:
+private:
   void send_command(const char *command, const char *parameter) {
     conn.write("AT+");
     conn.write(command);
@@ -57,14 +76,16 @@ class Bluetooth {
 };
 
 class Bg {
- private:
+private:
   static Bg *tasks_[N_TASKS];
   static char curr_task_;
 
- public:
+public:
   Bg(long interval)
-    : interval_(interval)
-    , next_run_(millis()) {}
+: 
+    interval_(interval)
+      , next_run_(millis()) {
+      }
 
   virtual bool run() = 0;
 
@@ -72,10 +93,14 @@ class Bg {
     for (char i = 0; i < N_TASKS; i++) {
       if (!Bg::tasks_[i]) {
         Bg::tasks_[i] = task;
+        Serial.print('T');
+        Serial.println((int)i);
         return;
       }
     }
 
+    Serial.print('T');
+    Serial.println('?');
     delete task;
   }
 
@@ -89,16 +114,17 @@ class Bg {
     for (int task = 0; task < N_TASKS; task++)
       Bg::tasks_[task] = NULL;
   }
- private:
+private:
   static Bg *runTask(Bg *task, const long m) {
     if (task) {
       if (m >= task->next_run_) {
-         if (task->run()) {
-           task->next_run_ = task->interval_ + m;
-         } else {
-           delete task;
-           task = NULL;
-         }
+        if (task->run()) {
+          task->next_run_ = task->interval_ + m;
+        } 
+        else {
+          delete task;
+          task = NULL;
+        }
       }
     }
     return task;
@@ -111,32 +137,26 @@ class Bg {
 Bg *Bg::tasks_[N_TASKS];
 char Bg::curr_task_;
 
-// Generated with this Python program:
-// >>> def pwm(p):
-// ...   p = 0.5 + 0.64057897435 * exp(0.0599320084 * p)
-// ...   if p > 255: return 255
-// ...   if p < 0: return 0
-// ...   return int(p)
 static const unsigned char pwmFadeTable[] PROGMEM = {
-   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
-   2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5,
-   6, 6, 6, 7, 7, 7, 8, 8, 9, 10, 10, 11, 11, 12, 13, 14,
-   14, 15, 16, 17, 18, 20, 21, 22, 23, 25, 26, 28, 30, 32,
-   33, 36, 38, 40, 43, 45, 48, 51, 54, 57, 61, 65, 69, 73,
-   77, 82, 87, 93, 98, 104, 111, 118, 125, 133, 141, 150,
-   159, 169, 179, 190, 202, 214, 228, 242, 255
+  2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+  4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6,
+  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7,
+  7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 10, 20,
+  30, 40, 50, 80, 90, 100, 120, 150, 200, 255
 };
 
-class FadeBg : public Bg {
- public:
+class FadeBg : 
+public Bg {
+public:
   FadeBg(char pin, long duration, bool in)
-      : Bg(max(duration / 100, 1))
-      , pin_(pin)
-      , percentage_(in ? 0 : 100)
-      , in_(in) {}
+    : Bg(duration ? duration : 1)
+    , pin_(pin)
+    , percentage_(in ? 0 : 100)
+    , in_(in) {}
 
   virtual bool run();
- private:
+private:
   char percentage_;
   char pin_;
   bool in_;
@@ -157,21 +177,31 @@ bool FadeBg::run() {
   if (pin_ < 0) {
     analogWrite(elWire1Pin, val);
     analogWrite(elWire2Pin, val);
-  } else {
+  } 
+  else {
     analogWrite(pin_, val);
   }
+
+  Serial.print((int)percentage_);
+  Serial.print('%');
+  Serial.print('=');
+  Serial.println((int)val);
+
   return true;
 }
 
-class StrobeBg : public Bg {
- public:
+class StrobeBg : 
+public Bg {
+public:
   StrobeBg(char pin, long interval, char count)
-      : Bg(interval)
+: 
+    Bg(interval)
       , pin_(pin)
-      , count_(count * 2) {}
+        , count_(count * 2) {
+        }
 
   virtual bool run();
- private:
+private:
   char count_;
   char pin_;
 };
@@ -180,7 +210,8 @@ bool StrobeBg::run() {
   if (pin_ < 0) {
     digitalWrite(elWire1Pin, count_ & 1);
     digitalWrite(elWire2Pin, count_ & 1);
-  } else {
+  } 
+  else {
     digitalWrite(pin_, count_ & 1);
   }
   return --count_;
@@ -211,11 +242,13 @@ void setup()
   Serial.begin(9600);
   bluetooth.begin();
 
-  char dancer_number = EEPROM.read(0) % 10;
+  char dancer_number = EEPROM.read(dancerIdEepromAddress) % 10;
+  Serial.print("Dancer number: ");
+  Serial.println((int)dancer_number);
 
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  
+
   if (dancer_number == 2) {
     Serial.println("Workaround for dancer 2 fucked up ports");
     pinMode(5, INPUT);
@@ -254,86 +287,97 @@ void loop()
   int chr = bluetooth.read();
 
   switch (state) {
-    case IDLE:
-      blinkStatusLed();
-      if (chr == 'A') {
-        state = WAITING_ANALOG_ID;
-      } else if (chr == 'D') {
-        state = WAITING_DIGITAL_ID;
-      } else if (chr == 'T') {
-        testMode();
-      } else if (chr == 'R') {
-        reset();
-      } else if (chr == 'L') {
-        blinkStatusLed();
-      } else if (chr == 'S') {
-        state = WAITING_STROBE_ID;
-      } else if (chr == 'O') {
-        state = WAITING_FADE_OUT_ID;
-      } else if (chr == 'I') {
-        state = WAITING_FADE_IN_ID;
-      } else if (chr == 'N') {
-        state = WAITING_DANCER_NUMBER;
-      }
-      return;
-    case WAITING_DANCER_NUMBER:
-      EEPROM.write(0, chr % 10);
+  case IDLE:
+    //      blinkStatusLed();
+
+    if (chr == 'A') {
+      state = WAITING_ANALOG_ID;
+    } 
+    else if (chr == 'D') {
+      state = WAITING_DIGITAL_ID;
+    } 
+    else if (chr == 'T') {
+      testMode();
+    } 
+    else if (chr == 'R') {
       reset();
-      break;
-    case WAITING_FADE_OUT_ID:
-      id = chr;
-      state = isValidId() ? WAITING_FADE_OUT_VALUE : IDLE;
-      break;
-    case WAITING_FADE_OUT_VALUE:
-      Bg::addTask(new FadeBg(idToPin(id), chr, false));
-      state = IDLE;
-      break;
-    case WAITING_FADE_IN_ID:
-      id = chr;
-      state = isValidId() ? WAITING_FADE_IN_VALUE : IDLE;
-      break;
-    case WAITING_FADE_IN_VALUE:
-      Bg::addTask(new FadeBg(idToPin(id), chr, true));
-      state = IDLE;
-      break;
-    case WAITING_STROBE_ID:
-      id = chr;
-      state = isValidId() ? WAITING_STROBE_VALUE1 : IDLE;
-      break;
-    case WAITING_STROBE_VALUE1:
-      value = chr;
-      state = WAITING_STROBE_VALUE2;
-      break;
-    case WAITING_STROBE_VALUE2:
-      Bg::addTask(new StrobeBg(idToPin(id), value, chr));
-      state = IDLE;
-      break;
-    case WAITING_ANALOG_ID:
-      id = chr;
-      state = isValidId() ? WAITING_ANALOG_VALUE : IDLE;
-      return;
-    case WAITING_ANALOG_VALUE:
-      if (id == 2) {
-        analogWrite(idToPin(0), constrain(chr, 0, 255));
-        analogWrite(idToPin(1), constrain(chr, 0, 255));
-      } else {
-        analogWrite(idToPin(id), constrain(chr, 0, 255));
-      }
-      state = IDLE;
-      break;
-    case WAITING_DIGITAL_ID:
-      id = chr;
-      state = isValidId() ? WAITING_DIGITAL_VALUE : IDLE;
-      return;
-    case WAITING_DIGITAL_VALUE:
-      if (id == 2) {
-        digitalWrite(idToPin(0), !!chr);
-        digitalWrite(idToPin(1), !!chr);
-      } else {
-        digitalWrite(idToPin(id), !!chr);
-      }
-      state = IDLE;
-      break;
+    } 
+    else if (chr == 'L') {
+      blinkStatusLed();
+    } 
+    else if (chr == 'S') {
+      state = WAITING_STROBE_ID;
+    } 
+    else if (chr == 'O') {
+      state = WAITING_FADE_OUT_ID;
+    } 
+    else if (chr == 'I') {
+      state = WAITING_FADE_IN_ID;
+    } 
+    else if (chr == 'N') {
+      state = WAITING_DANCER_NUMBER;
+    }
+    return;
+  case WAITING_DANCER_NUMBER:
+    EEPROM.write(dancerIdEepromAddress, chr % 10);
+    reset();
+    break;
+  case WAITING_FADE_OUT_ID:
+    id = chr;
+    state = isValidId() ? WAITING_FADE_OUT_VALUE : IDLE;
+    break;
+  case WAITING_FADE_OUT_VALUE:
+    Bg::addTask(new FadeBg(idToPin(id), chr, false));
+    state = IDLE;
+    break;
+  case WAITING_FADE_IN_ID:
+    id = chr;
+    state = isValidId() ? WAITING_FADE_IN_VALUE : IDLE;
+    break;
+  case WAITING_FADE_IN_VALUE:
+    Bg::addTask(new FadeBg(idToPin(id), chr, true));
+    state = IDLE;
+    break;
+  case WAITING_STROBE_ID:
+    id = chr;
+    state = isValidId() ? WAITING_STROBE_VALUE1 : IDLE;
+    break;
+  case WAITING_STROBE_VALUE1:
+    value = chr;
+    state = WAITING_STROBE_VALUE2;
+    break;
+  case WAITING_STROBE_VALUE2:
+    Bg::addTask(new StrobeBg(idToPin(id), value, chr));
+    state = IDLE;
+    break;
+  case WAITING_ANALOG_ID:
+    id = chr;
+    state = isValidId() ? WAITING_ANALOG_VALUE : IDLE;
+    return;
+  case WAITING_ANALOG_VALUE:
+    if (id == 2) {
+      analogWrite(idToPin(0), constrain(chr, 0, 255));
+      analogWrite(idToPin(1), constrain(chr, 0, 255));
+    } 
+    else {
+      analogWrite(idToPin(id), constrain(chr, 0, 255));
+    }
+    state = IDLE;
+    break;
+  case WAITING_DIGITAL_ID:
+    id = chr;
+    state = isValidId() ? WAITING_DIGITAL_VALUE : IDLE;
+    return;
+  case WAITING_DIGITAL_VALUE:
+    if (id == 2) {
+      digitalWrite(idToPin(0), !!chr);
+      digitalWrite(idToPin(1), !!chr);
+    } 
+    else {
+      digitalWrite(idToPin(id), !!chr);
+    }
+    state = IDLE;
+    break;
   }
 }
 
@@ -350,9 +394,9 @@ int idToPin(int id) {
 }
 
 void testModeBlinkInternal(int el1Mode, int el2Mode) {
-   digitalWrite(elWire1Pin, el1Mode);
-   digitalWrite(elWire2Pin, el2Mode);
-   delay(200);
+  digitalWrite(elWire1Pin, el1Mode);
+  digitalWrite(elWire2Pin, el2Mode);
+  delay(200);
 }
 
 void testMode() {
@@ -387,4 +431,5 @@ void blinkStatusLed() {
     delay(100);
   }
 }
+
 
